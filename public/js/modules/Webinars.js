62d1adc885ba45f601c5e8c1d9fe1bc0b151bb64
skoplev <skoplev@gmail.com>
2016-03-06 20:04:20 -0500

    Webinar overview directive and upcoming webinar section.

--- public/js/modules/Webinars.js
+++ public/js/modules/Webinars.js
@@ -3,13 +3,14 @@ var mod = angular.module("Webinars", []);
 mod.controller("WebinarsCtrl", ["$scope", "$http", "$sce", "$timeout", function($scope, $http, $sce, $timeout) {
 	$scope.webinars = [];
 
+	$scope.id_counter = 0;
+
 	$http.get("/content/webinars.json")
 		.success(function(data) {
-			// Sanitize embed urls, and trust abstracts as html.
-			for (var i in data) {
+			// Trust abstracts as html.
+			for (var i in data.past) {
 				try {
-					data[i].embed = $sce.trustAsResourceUrl(data[i].embed);
-					data[i].abstract = $sce.trustAsHtml(data[i].abstract)
+					data.past[i].abstract = $sce.trustAsHtml(data.past[i].abstract)
 				}
 				catch(err) {
 					console.warn(err);
@@ -22,9 +23,20 @@ mod.controller("WebinarsCtrl", ["$scope", "$http", "$sce", "$timeout", function(
 		.error(function(data) {
 			console.error(data);
 		});
-
-	// Toggle element on page
-	$scope.toggle = function(id) {
-		$("#" + id).toggleClass("hide");
-	}
 }])
+
+mod.directive("webinar", function() {
+	return {
+		scope: {
+			data: "="
+		},
+		restrict: "A",
+		templateUrl: "templates/WebinarTemp.html",
+		controller: ["$scope", "$element", function($scope, $element) {
+			// Toggle element on page
+			$scope.toggleAbstract = function() {
+				$element.find("#abstract").toggleClass("hide");
+			}
+		}]
+	}
+})
